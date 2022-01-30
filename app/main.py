c21178ade3dc4128278779382a21558935d81233
dongh3930 <dongh3930@naver.com>
2022-01-30 21:46:16 +0900

    commit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import json
+import folium
+import matplotlib.pyplot as plt
 import dash
 from dash import dcc
 from dash import html
@@ -9,16 +12,20 @@ import pandas as pd
 app=dash.Dash(__name__)
 server = app.server
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df= pd.read_excel('kdn.xls',header=0)
+df = df.set_index('자치구')
 
-fig=px.bar(df,x="Fruit",y="Amount",color="City",barmode="group")
+#서울시 '구'별 경계선을 그리기 위한 json파일 로딩
+geo_path = '02. skorea_municipalities_geo_simple.json'
+geo_str = json.load(open(geo_path, encoding='utf-8'))
+
+#서울시 중심의 위도와 경도, 확대 비율, 지도 모양 설정
+map = folium.Map(location=[37.5502, 126.982], zoom_start=11, tiles='openstreetmap')
+
+fig = map.choropleth(geo_data=geo_str, data = df['인구당전력'],columns=[df.index, df['인구당전력']], fill_color='PuRd', key_on='feature.id')
 
 app.layout = html.Div(children=[
-    html.H1(children='1인당 전력사용량과 자살율의 상관관계'),
+    html.H1(children='서울특별시 자치구별 인구당 전력 사용량'),
 
     html.Div(children='''
         Dash:A web application framework for your data.
@@ -27,7 +34,6 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure=fig
     )
-
 ])
 
 if __name__ =='__main__':
