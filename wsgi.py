7a84e0ab94d4f251f47835e75aa7d4476e8a61c7
dongh3930 <dongh3930@naver.com>
2022-01-30 20:32:32 +0900

    commit

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app.main import app
 import os 
 
-port = int(os.environ.get("PORT", 5000))
+server = app.server
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=port, debug=True)
+        app.run_server(host='0.0.0.0', port=port, debug=True)
